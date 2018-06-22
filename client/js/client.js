df24719184ccd2de1ff943db0e33b9c51831aab4
Mário Silva <mario156silva@gmail.com>
2018-06-22 16:27:45 +0100

    File transfer working between any two peers

--- client/js/client.js
+++ client/js/client.js
@@ -136,7 +136,7 @@ async function updateDirectMessages() {
                         var username = value.messages[i].message.split(";")[1].split(":")[0];
                         var message = value.messages[i].message.split(";")[1].split(":")[1];
                         if(value.messages[i].type == "file") {
-                            text = "<a class='file " + hashCode(message) + "'>" + message + "</a>";
+                            text = "<a class='file " + MD5(message) + "'>Receiving...</a>";
                         }
                         else if(value.messages[i].type == "text"){
                             text = message;
--- client/js/webrtc.js
+++ client/js/webrtc.js
@@ -66,15 +66,215 @@ function IsJsonString(str) {
 }
 
 // Return a hash of a string
-function hashCode(str) {
-    var hash = 0, i, chr;
-    if (str.length === 0) return hash;
-    for (i = 0; i < str.length; i++) {
-        chr   = str.charCodeAt(i);
-        hash  = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
+var MD5 = function (string) {
+
+    function RotateLeft(lValue, iShiftBits) {
+        return (lValue<<iShiftBits) | (lValue>>>(32-iShiftBits));
     }
-    return hash;
+
+    function AddUnsigned(lX,lY) {
+        var lX4,lY4,lX8,lY8,lResult;
+        lX8 = (lX & 0x80000000);
+        lY8 = (lY & 0x80000000);
+        lX4 = (lX & 0x40000000);
+        lY4 = (lY & 0x40000000);
+        lResult = (lX & 0x3FFFFFFF)+(lY & 0x3FFFFFFF);
+
+        if (lX4 & lY4) {
+            return (lResult ^ 0x80000000 ^ lX8 ^ lY8);
+        }
+
+        if (lX4 | lY4) {
+            if (lResult & 0x40000000) {
+                return (lResult ^ 0xC0000000 ^ lX8 ^ lY8);
+            }
+            else {
+                return (lResult ^ 0x40000000 ^ lX8 ^ lY8);
+            }
+        }
+        else {
+            return (lResult ^ lX8 ^ lY8);
+        }
+    }
+
+    function F(x,y,z) { return (x & y) | ((~x) & z); }
+
+    function G(x,y,z) { return (x & z) | (y & (~z)); }
+
+    function H(x,y,z) { return (x ^ y ^ z); }
+
+    function I(x,y,z) { return (y ^ (x | (~z))); }
+
+    function FF(a,b,c,d,x,s,ac) {
+        a = AddUnsigned(a, AddUnsigned(AddUnsigned(F(b, c, d), x), ac));
+
+        return AddUnsigned(RotateLeft(a, s), b);
+    };
+
+    function GG(a,b,c,d,x,s,ac) {
+        a = AddUnsigned(a, AddUnsigned(AddUnsigned(G(b, c, d), x), ac));
+
+        return AddUnsigned(RotateLeft(a, s), b);
+    };
+
+    function HH(a,b,c,d,x,s,ac) {
+        a = AddUnsigned(a, AddUnsigned(AddUnsigned(H(b, c, d), x), ac));
+
+        return AddUnsigned(RotateLeft(a, s), b);
+    };
+
+    function II(a,b,c,d,x,s,ac) {
+        a = AddUnsigned(a, AddUnsigned(AddUnsigned(I(b, c, d), x), ac));
+
+        return AddUnsigned(RotateLeft(a, s), b);
+    };
+
+    function ConvertToWordArray(string) {
+        var lWordCount;
+        var lMessageLength = string.length;
+        var lNumberOfWords_temp1=lMessageLength + 8;
+        var lNumberOfWords_temp2=(lNumberOfWords_temp1-(lNumberOfWords_temp1 % 64))/64;
+        var lNumberOfWords = (lNumberOfWords_temp2+1)*16;
+        var lWordArray=Array(lNumberOfWords-1);
+        var lBytePosition = 0;
+        var lByteCount = 0;
+
+        while ( lByteCount < lMessageLength ) {
+            lWordCount = (lByteCount-(lByteCount % 4))/4;
+            lBytePosition = (lByteCount % 4)*8;
+            lWordArray[lWordCount] = (lWordArray[lWordCount] | (string.charCodeAt(lByteCount)<<lBytePosition));
+            lByteCount++;
+        }
+        lWordCount = (lByteCount-(lByteCount % 4))/4;
+        lBytePosition = (lByteCount % 4)*8;
+        lWordArray[lWordCount] = lWordArray[lWordCount] | (0x80<<lBytePosition);
+        lWordArray[lNumberOfWords-2] = lMessageLength<<3;
+        lWordArray[lNumberOfWords-1] = lMessageLength>>>29;
+
+        return lWordArray;
+    };
+
+    function WordToHex(lValue) {
+        var WordToHexValue="",WordToHexValue_temp="",lByte,lCount;
+
+        for (lCount = 0;lCount<=3;lCount++) {
+            lByte = (lValue>>>(lCount*8)) & 255;
+            WordToHexValue_temp = "0" + lByte.toString(16);
+            WordToHexValue = WordToHexValue + WordToHexValue_temp.substr(WordToHexValue_temp.length-2,2);
+        }
+        return WordToHexValue;
+    };
+
+    function Utf8Encode(string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return utftext;
+    };
+
+    var x=Array();
+    var k,AA,BB,CC,DD,a,b,c,d;
+    var S11=7, S12=12, S13=17, S14=22;
+    var S21=5, S22=9 , S23=14, S24=20;
+    var S31=4, S32=11, S33=16, S34=23;
+    var S41=6, S42=10, S43=15, S44=21;
+    string = Utf8Encode(string);
+    x = ConvertToWordArray(string);
+    a = 0x67452301;
+    b = 0xEFCDAB89;
+    c = 0x98BADCFE;
+    d = 0x10325476;
+
+    for (k=0;k<x.length;k+=16) {
+        AA=a; BB=b; CC=c; DD=d;
+        a=FF(a,b,c,d,x[k+0], S11,0xD76AA478);
+        d=FF(d,a,b,c,x[k+1], S12,0xE8C7B756);
+        c=FF(c,d,a,b,x[k+2], S13,0x242070DB);
+        b=FF(b,c,d,a,x[k+3], S14,0xC1BDCEEE);
+        a=FF(a,b,c,d,x[k+4], S11,0xF57C0FAF);
+        d=FF(d,a,b,c,x[k+5], S12,0x4787C62A);
+        c=FF(c,d,a,b,x[k+6], S13,0xA8304613);
+        b=FF(b,c,d,a,x[k+7], S14,0xFD469501);
+        a=FF(a,b,c,d,x[k+8], S11,0x698098D8);
+        d=FF(d,a,b,c,x[k+9], S12,0x8B44F7AF);
+        c=FF(c,d,a,b,x[k+10],S13,0xFFFF5BB1);
+        b=FF(b,c,d,a,x[k+11],S14,0x895CD7BE);
+        a=FF(a,b,c,d,x[k+12],S11,0x6B901122);
+        d=FF(d,a,b,c,x[k+13],S12,0xFD987193);
+        c=FF(c,d,a,b,x[k+14],S13,0xA679438E);
+        b=FF(b,c,d,a,x[k+15],S14,0x49B40821);
+        a=GG(a,b,c,d,x[k+1], S21,0xF61E2562);
+        d=GG(d,a,b,c,x[k+6], S22,0xC040B340);
+        c=GG(c,d,a,b,x[k+11],S23,0x265E5A51);
+        b=GG(b,c,d,a,x[k+0], S24,0xE9B6C7AA);
+        a=GG(a,b,c,d,x[k+5], S21,0xD62F105D);
+        d=GG(d,a,b,c,x[k+10],S22,0x2441453);
+        c=GG(c,d,a,b,x[k+15],S23,0xD8A1E681);
+        b=GG(b,c,d,a,x[k+4], S24,0xE7D3FBC8);
+        a=GG(a,b,c,d,x[k+9], S21,0x21E1CDE6);
+        d=GG(d,a,b,c,x[k+14],S22,0xC33707D6);
+        c=GG(c,d,a,b,x[k+3], S23,0xF4D50D87);
+        b=GG(b,c,d,a,x[k+8], S24,0x455A14ED);
+        a=GG(a,b,c,d,x[k+13],S21,0xA9E3E905);
+        d=GG(d,a,b,c,x[k+2], S22,0xFCEFA3F8);
+        c=GG(c,d,a,b,x[k+7], S23,0x676F02D9);
+        b=GG(b,c,d,a,x[k+12],S24,0x8D2A4C8A);
+        a=HH(a,b,c,d,x[k+5], S31,0xFFFA3942);
+        d=HH(d,a,b,c,x[k+8], S32,0x8771F681);
+        c=HH(c,d,a,b,x[k+11],S33,0x6D9D6122);
+        b=HH(b,c,d,a,x[k+14],S34,0xFDE5380C);
+        a=HH(a,b,c,d,x[k+1], S31,0xA4BEEA44);
+        d=HH(d,a,b,c,x[k+4], S32,0x4BDECFA9);
+        c=HH(c,d,a,b,x[k+7], S33,0xF6BB4B60);
+        b=HH(b,c,d,a,x[k+10],S34,0xBEBFBC70);
+        a=HH(a,b,c,d,x[k+13],S31,0x289B7EC6);
+        d=HH(d,a,b,c,x[k+0], S32,0xEAA127FA);
+        c=HH(c,d,a,b,x[k+3], S33,0xD4EF3085);
+        b=HH(b,c,d,a,x[k+6], S34,0x4881D05);
+        a=HH(a,b,c,d,x[k+9], S31,0xD9D4D039);
+        d=HH(d,a,b,c,x[k+12],S32,0xE6DB99E5);
+        c=HH(c,d,a,b,x[k+15],S33,0x1FA27CF8);
+        b=HH(b,c,d,a,x[k+2], S34,0xC4AC5665);
+        a=II(a,b,c,d,x[k+0], S41,0xF4292244);
+        d=II(d,a,b,c,x[k+7], S42,0x432AFF97);
+        c=II(c,d,a,b,x[k+14],S43,0xAB9423A7);
+        b=II(b,c,d,a,x[k+5], S44,0xFC93A039);
+        a=II(a,b,c,d,x[k+12],S41,0x655B59C3);
+        d=II(d,a,b,c,x[k+3], S42,0x8F0CCC92);
+        c=II(c,d,a,b,x[k+10],S43,0xFFEFF47D);
+        b=II(b,c,d,a,x[k+1], S44,0x85845DD1);
+        a=II(a,b,c,d,x[k+8], S41,0x6FA87E4F);
+        d=II(d,a,b,c,x[k+15],S42,0xFE2CE6E0);
+        c=II(c,d,a,b,x[k+6], S43,0xA3014314);
+        b=II(b,c,d,a,x[k+13],S44,0x4E0811A1);
+        a=II(a,b,c,d,x[k+4], S41,0xF7537E82);
+        d=II(d,a,b,c,x[k+11],S42,0xBD3AF235);
+        c=II(c,d,a,b,x[k+2], S43,0x2AD7D2BB);
+        b=II(b,c,d,a,x[k+9], S44,0xEB86D391);
+        a=AddUnsigned(a,AA);
+        b=AddUnsigned(b,BB);
+        c=AddUnsigned(c,CC);
+        d=AddUnsigned(d,DD);
+    }
+
+    var temp = WordToHex(a)+WordToHex(b)+WordToHex(c)+WordToHex(d);
+
+    return temp.toLowerCase();
 }
 
 // Create promise to sleep for ms miliseconds
@@ -245,24 +445,31 @@ function handleClientMessage(event) {
 
         if (receivedSize == fileSize) {
             var received = new window.Blob(receiveBuffer);
+            console.log("All received");
             handleFile(received, fileDate, fileName, fileType, fileSize, fileSrc, fileDst);
         }
     }
 }
 
-async function handleFile(buffer, date, fileName, type, size, src, dst) {
+async function handleFile(buffer, date, fName, type, size, src, dst) {
+    console.log(name + " - " + fileDst);
     if(dst==name) {
+        console.log("Yup");
         receiveBuffer = [];
         receivedSize = 0;
 
         var href = URL.createObjectURL(buffer);
-        var download = fileName;
-        var textContent = fileName;
-        var hash = hashCode(fileName);
+        var download = fName;
+        var textContent = fName;
+        var hash = MD5(fName);
 
-        $('a.' + hash).attr('href', href);
-        $('a.' + hash).attr('download', download);
-        $('a.' + hash).html(textContent);
+        console.log($('.' + hash).length);
+        if($('.' + hash).length == 0) {
+            await sleep(1000);
+        }
+        $('.' + hash).attr('href', href);
+        $('.' + hash).attr('download', download);
+        $('.' + hash).html(textContent);
 
         fileName = "";
         fileType = "";
@@ -272,11 +479,8 @@ async function handleFile(buffer, date, fileName, type, size, src, dst) {
         fileDate = "";
     }
     else {
-        var msg = dst + ";" + date + ";" + src + ";" + fileName + ";" + size + ";" + type;
+        var msg = dst + ";" + date + ";" + src + ";" + fName + ";" + size + ";" + type;
         handleDirectMessage("file", dst, msg);
-        if(!connections.has(dst) || !reachable.has(dst)) {
-            await sleep(10000);
-        }
         sendFile(dst, buffer);
         fileName = "";
         fileType = "";
@@ -293,18 +497,21 @@ async function handleFile(buffer, date, fileName, type, size, src, dst) {
 function handleReplyRoute(snd, src, dest) {
     var next;
     if(reachable.has(dest) && typeof reachable.get(dest) == "object") {
-        next = reachable.get(dest).destination;
-        if(next != "") {
-            send({
-                type: "reply",
-                sender: name,
-                source: src,
-                destination: dest
-            }, next);
+        if(reachable.get(dest).time == -1) {
+            console.log("Updated Reachable " + snd);
+            var t = new Date();
+            next = reachable.get(dest).destination;
+            // Update reachable table
+            reachable.set(dest, {destination: snd, time: t.getTime()});
+            if(next != "") {
+                send({
+                    type: "reply",
+                    sender: name,
+                    source: src,
+                    destination: dest
+                }, next);
+            }
         }
-        // Update reachable table
-        var t = new Date();
-        reachable.set(dest, {destination: snd, time: t.getTime()});
     }
 }
 
@@ -384,6 +591,7 @@ async function handleDirectMessage(src, dest, msg) {
             temp.type = "file";
         }
         if(connections.has(dest)) {
+            console.log("Destination: " + dest + " Sent: " + msg);
             connections.get(dest).channel.send(JSON.stringify(temp));
         }
         else if(reachable.has(dest)) {
@@ -391,10 +599,10 @@ async function handleDirectMessage(src, dest, msg) {
             // If the entry on reachable is object means that it was added by a reply to a route request
             if(typeof obj == "object") {
                 var t = new Date();
+                var trys = 0;
                 // If entry is more than 5 minutes old it gets refreshed
-                if((t.getTime() - obj.time)>300000) {
-                    reachable.delete(dest);
-                    requests.set(dest, name);
+                if(obj.time != -1 && (t.getTime() - obj.time) > 300000) {
+                    reachable.set(dest, {destination: "", time: -1});
                     var tempReq = {
                         type: "request",
                         sender: name,
@@ -404,20 +612,32 @@ async function handleDirectMessage(src, dest, msg) {
                     for(var con of connections.values()){
                         con.channel.send(JSON.stringify(tempReq));
                     }
-                    var trys = 0;
-                    while(!reachable.has(dest) && trys < 3) {
-                        await sleep(3000);
+                    while(reachable.get(dest).time == -1 && trys < 18) {
+                        await sleep(500);
                         trys++;
                     }
-                    if(trys < 3) {
-                        connections.get(reachable.get(dest)).channel.send(JSON.stringify(temp));
+                    if(trys < 18) {
+                        console.log("Destination: " + connections.get(reachable.get(dest).destination).name + " Sent: " + msg);
+                        connections.get(reachable.get(dest).destination).channel.send(JSON.stringify(temp));
+                    }
+                }
+                else if(obj.time == -1) {
+                    while(reachable.get(dest).time == -1 && trys < 18) {
+                        await sleep(500);
+                        trys++;
+                    }
+                    if(trys < 18) {
+                        console.log("Destination: " + connections.get(reachable.get(dest).destination).name + " Sent: " + msg);
+                        connections.get(reachable.get(dest).destination).channel.send(JSON.stringify(temp));
                     }
                 }
                 else {
+                    console.log("Destination: " + connections.get(obj.destination).name + " Sent: " + msg);
                     connections.get(obj.destination).channel.send(JSON.stringify(temp));
                 }
             }
             else {
+                console.log("Destination: " + obj + " Sent: " + msg);
                 connections.get(obj).channel.send(JSON.stringify(temp));
             }
         }
@@ -434,11 +654,12 @@ async function handleDirectMessage(src, dest, msg) {
                 con.channel.send(JSON.stringify(tempReq));
             }
             var trys = 0;
-            while(reachable.get(dest).time == -1 && trys < 3) {
-                await sleep(3000);
+            while(reachable.get(dest).time == -1 && trys < 18) {
+                await sleep(500);
                 trys++;
             }
-            if(trys < 3) {
+            if(trys < 18) {
+                console.log("Destination: " + connections.get(reachable.get(dest).destination).name + " Sent: " + msg);
                 connections.get(reachable.get(dest).destination).channel.send(JSON.stringify(temp));
             }
         }
@@ -495,6 +716,7 @@ function handleLogin(success, peers) {
     }
     else {
         logged = true;
+        $('title').html(name); // apagar
         var us = peers.split(";");
         for(i = 0; i < us.length-1; i++) {
             if(availablePeers.indexOf(us[i]) == -1 && us[i] != name) {
@@ -739,14 +961,40 @@ async function sendMessage(type, dest, msg) {
 
 
 async function sendFile(dst, file) {
+    // Don't remove this line
+    // This allows handleDirectMessage to create an entry in reachable Map
+    // if necessary
+    await sleep(900);
+    console.log("Entrou sendfile");
+    console.log("Destination: " + dst);
     var channel = 1;
     if(connections.has(dst)) {
+        console.log("Nas connections");
+        console.log("file to: " + connections.get(dst).name);
         channel = connections.get(dst).channel;
     }
     else if(reachable.has(dst)) {
-        channel = connections.get(reachable.get(dst)).channel;
+        console.log("No reachable");
+        var obj = reachable.get(dst);
+        // If the entry on reachable is object means that it was added by a reply to a route request
+        if(typeof obj == "object") {
+            var trys = 0;
+            while(reachable.get(dst).time == -1 && trys < 4) {
+                await sleep(3000);
+                trys++;
+            }
+            if(trys < 4) {
+                channel = connections.get(reachable.get(dst).destination).channel
+                console.log("file to: " + connections.get(reachable.get(dst).destination).name);
+            }
+        }
+        else {
+            console.log("file to: " + connections.get(reachable.get(dst)).name);
+            channel = connections.get(reachable.get(dst)).channel
+        }
     }
     if(channel!==1) {
+        console.log("vai enviar");
         var sliceFile = function(offset) {
             var reader = new window.FileReader();
             reader.onload = (function() {
